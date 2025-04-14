85483783b92730e74cce94625aac417d9d83e098
Upload_project <tatianaeglit1977@gmail.com>
2025-04-14 16:47:26 +0300

    [feature/Lesson_1]: Learn Java Variables

--- src/main/java/lv/acodemy/MyClass.java
+++ src/main/java/lv/acodemy/MyClass.java
@@ -4,34 +4,30 @@ public class MyClass {
     public static void main(String[] args) {
         System.out.println("Ja u Mami programmist");
 
-        //byte 8-bit
+        // byte 8-bit
         byte age = 30;
         System.out.println(age);
 
-        //Naming convention:
-        //Camel Case: MyClass (Capital Camel Case)
-        //Camel Case: currentYear for variables (Lower Camel Case )
-
-        //short 16-bit
+        // short 16-bit
         short currentYear = 2025;
         short temperature = -273;
         short distance = 1688;
 
-        //int 32-bit
+        // int 32-bit
         int score = 9000;
         int population = 144400000;
         int result = (10 * 10) - 42;
         System.out.println(result);
 
         // long 64-bit
-        long starsInGalaxy = 100_000_000L; // 100000000000
-        long bigNumber = 999998787767565L;
+        long starsInGalaxy = 100_000_000L;
+        long bigNumber = 999_998_787_767_565L;
         long currentMillis = System.currentTimeMillis();
         System.out.println(currentMillis);
 
         // float 32-bit
         float pi = 3.14f;
-        float piNumber = (float) Math.PI;// casting
+        float piNumber = (float) Math.PI;
         float discount = 25.5f;
 
         // double 64-bit
@@ -43,22 +39,23 @@ public class MyClass {
         System.out.println(pi);
         System.out.println(piDouble);
 
-        //char  (symbol)
+        // char
         char initial = 'N';
         char digit = '7';
         char symbol = '#';
 
-        // Sie unicode table
-        char smiley = 'W263A';
+        // Unicode symbol ☺
+        char smiley = '\u263A';
         System.out.println(smiley);
 
-        // boolean (true/false)
+        // boolean
         boolean isJavaFun = true;
-        boolean passed = (40 > 60);//false
+        boolean passed = (40 > 60);
 
         // String
-        String myFyllName = 'Nikita Milka';
-
-
+        String myFullName = "Nikita Milka";
+        System.out.println(myFullName);
     }
 }
+
+
